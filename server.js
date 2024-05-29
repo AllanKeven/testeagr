958ea47ed4aad62b733a640d0bc2fa91b589803e
Allan <alencarmesquitaallankeven@gmail.com>
2024-05-29 15:03:44 -0300

    comitando

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+app.get('/cep/:cep', async (req, res) => {
+  const { cep } = req.params;
+
+  try {
+    const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar o CEP' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
